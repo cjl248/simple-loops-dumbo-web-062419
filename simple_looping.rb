beb59a3a2af31e12afea9d094a58e23feac567c1
Chris Liendo <cjl248@cornell.edu>
2019-06-20 08:31:46 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -61,7 +61,7 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
-  for counter in 0..number_of_times do
+  for counter in 0...number_of_times do
     puts phrase
   end
 end
